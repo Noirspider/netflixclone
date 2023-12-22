a415afd2f5215473ebcee138d20f971b8433b473
Noirspider <83585087+Noirspider@users.noreply.github.com>
2023-12-22 18:24:11 +0100

    correzione footer e slider

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,61 +3,69 @@ import "../style.css";
 
 const MyFooter = () => {
   return (
-    <footer style={{ backgroundColor: "black" }} className="mt-5 text-center">
-      <div className="social-media-icons mb-2">
-        <i className="fa fa-facebook footer-icon text-white"></i>
-        <i className="fa fa-instagram footer-icon"></i>
-        <i className="fa fa-twitter footer-icon"></i>
-        <i className="fa fa-youtube footer-icon"></i>
+    <footer style={{ backgroundColor: "black", color: "white" }} className="mt-5  text-center">
+      <div className="social-media-icons mb-2 text-white">
+        <i className="bi bi-facebook icon-spacing"></i>
+        <i className="bi bi-instagram icon-spacing"></i>
+        <i className="bi bi-twitter-x icon-spacing"></i>
+        <i className="bi bi-youtube icon-spacing"></i>
       </div>
 
       <div className="footer-content row justify-content-center">
-        <div className="col-12 col-sm-6 col-md-3 footer-links">
-          <p>
-            <button>Audio and Subtitles</button>
-          </p>
-          <p>
-            <button>Media Center</button>
-          </p>
-          <p>
-            <button>Privacy</button>
-          </p>
-          <p>
-            <button>Contact us</button>
-          </p>
+        <div className="col-12 col-sm-6 col-md-3 footer-links text-center">
+          <ul>
+            <li>
+              <a href="#">Audio and Subtitles</a>
+            </li>
+            <li>
+              <a href="#">Media Center</a>
+            </li>
+            <li>
+              <a href="#">Privacy</a>
+            </li>
+            <li>
+              <a href="#">Contact us</a>
+            </li>
+          </ul>
         </div>
-        <div className="col-12 col-sm-6 col-md-3 footer-links">
-          <p>
-            <button>Audio Description</button>
-          </p>
-          <p>
-            <button>Investor Relations</button>
-          </p>
-          <p>
-            <button>Legal Notices</button>
-          </p>
+        <div className="col-12 col-sm-6 col-md-3 footer-links text-center">
+          <ul>
+            <li>
+              <a href="#">Audio Description</a>
+            </li>
+            <li>
+              <a href="#">Investor Relations</a>
+            </li>
+            <li>
+              <a href="#">Legal Notices</a>
+            </li>
+          </ul>
         </div>
-        <div className="col-12 col-sm-6 col-md-3 footer-links">
-          <p>
-            <button>Help Center</button>
-          </p>
-          <p>
-            <button>Jobs</button>
-          </p>
-          <p>
-            <button>Cookie Preferences</button>
-          </p>
+        <div className="col-12 col-sm-6 col-md-3 footer-links text-center ">
+          <ul>
+            <li>
+              <a href="#">Help Center</a>
+            </li>
+            <li>
+              <a href="#">Jobs</a>
+            </li>
+            <li>
+              <a href="#">Cookie Preferences</a>
+            </li>
+          </ul>
         </div>
-        <div className="col-12 col-sm-6 col-md-3 footer-links">
-          <p>
-            <button>Gift Cards</button>
-          </p>
-          <p>
-            <button>Terms of Use</button>
-          </p>
-          <p>
-            <button>Corporate Information</button>
-          </p>
+        <div className="col-12 col-sm-6 col-md-3 footer-links text-center">
+          <ul>
+            <li>
+              <a href="#">Gift Cards</a>
+            </li>
+            <li>
+              <a href="#">Terms of Use</a>
+            </li>
+            <li>
+              <a href="#">Corporate Information</a>
+            </li>
+          </ul>
         </div>
       </div>
 
--- src/components/SingleMovie.jsx
+++ src/components/SingleMovie.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const SingleMovie = (props) => {
-  return <img src={props.img} className="poster" alt="description" style={{ width: "100%", height: "auto" }} />;
+  return (
+    <img
+      src={props.img}
+      className="poster"
+      alt="description"
+      style={{ width: "95%", height: "400px", objectFit: "cover" }}
+    />
+  );
 };
 
 export default SingleMovie;
